d0c8f727fd93ee95ef7de17f775b0e39bba75688
rajan_1905@live.com <rajan_1905@live.com>
2019-06-29 23:47:21 +0100

    Rock Paper Scissor Assignment

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,10 +4,13 @@ import game.GameUsingCompletableFuture;
 
 public class Application {
     public static void main(String[] args){
+        // Implementation With Completable Future
         /*GameUsingFuture gameUsingFuture = new GameUsingFuture(5);
         gameUsingFuture.startGame();*/
+        // Implementation Using Future
         /*GameUsingCompletableFuture gameUsingFuture = new GameUsingCompletableFuture(5);
         gameUsingFuture.startGame();*/
+        // Implementation using Threads
         GameUsingThread gameUsingThread = new GameUsingThread(5);
         gameUsingThread.startGame();
     }
